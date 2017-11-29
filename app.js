ea7409bd419d417ff271db4af9986d0f4aabca31
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-11-28 20:35:18 -0500

    completed exercise centuryFromYear

--- app.js
+++ app.js
@@ -10,3 +10,15 @@ add(param1, param2) = 3. */
 function add(param1, param2) {
     return param1 + param2;
 }
+
+//Challenge 2 - centuryFromYear
+
+/* Given a year, return the century it is in.
+The first century spans from the year 1 up to
+and including the year 100, the second - from
+the year 101 up to and including the year 200,
+etc.*/
+
+function centuryFromYear(year) {
+    return Math.ceil(year / 100);
+}
