85c5291c86fb42fd4c31f42dccc2f02f22fb51cb
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-11-28 20:35:38 -0500

    completed exercise checkPalindrome

--- app.js
+++ app.js
@@ -22,3 +22,26 @@ etc.*/
 function centuryFromYear(year) {
     return Math.ceil(year / 100);
 }
+
+//Challenge 3 - checkPalindrome
+
+/* Given the string, check if it is a palindrome.
+
+Example
+
+For inputString = "aabaa", the output should be
+checkPalindrome(inputString) = true;
+For inputString = "abac", the output should be
+checkPalindrome(inputString) = false;
+For inputString = "a", the output should be
+checkPalindrome(inputString) = true. */
+
+function checkPalindrome(inputString) {
+    if(inputString === inputString.split("").reverse().join("")){
+        return true;
+    }
+
+    else {
+        return false;
+    }
+}
